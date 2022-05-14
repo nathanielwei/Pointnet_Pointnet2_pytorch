a10ce68e470987f4a13b3bad2ba284ebeadd0e01
Nathaniel Zikai Wei <w.nathaniel.s@gmail.com>
2022-05-14 23:41:08 +0800

    Update ShapeNetDataLoader.py

--- data_utils/ShapeNetDataLoader.py
+++ data_utils/ShapeNetDataLoader.py
@@ -9,7 +9,7 @@ warnings.filterwarnings('ignore')
 def pc_normalize(pc):
     centroid = np.mean(pc, axis=0)
     pc = pc - centroid
-    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
+    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1))) # Nate: compute np.sqrt(x^2+y^2+z^2), find the largest one
     pc = pc / m
     return pc
 
